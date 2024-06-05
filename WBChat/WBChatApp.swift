ec2557552df39355bb86439dcd5afc866b3da8a7
QqewDev <kistalexander@yandex.ru>
2024-06-05 19:47:53 +0400

    Initial Commit

---
+++ WBChat/WBChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WBChatApp.swift
+//  WBChat
+//
+//  Created by Александр on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WBChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
