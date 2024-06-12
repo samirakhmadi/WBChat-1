565c6ae9aabcdbda9db8b84e4d08a4b7954e99d5
QqewDev <kistalexander@yandex.ru>
2024-06-12 19:23:16 +0400

    Создана кастомная анимация перехода на VC

---
+++ WBChat/Дополнительно💻/DestinationViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DestinationViewController.swift
+//  WBChat
+//
+//  Created by Александр on 12.06.2024.
+//
+
+import Foundation
+import UIKit
+
+final class DestinationVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Destination"
+        view.backgroundColor = .systemOrange
+        setupViews()
+    }
+    var backButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Вернуться", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemPurple
+        button.layer.masksToBounds = true
+        return button
+    }()
+    
+    var imageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "pc")
+        iv.tintColor = .systemRed
+        return iv
+    }()
+    
+    
+    private func setupViews() {
+        view.addSubview(backButton)
+        
+        NSLayoutConstraint.activate([
+            backButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            backButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            backButton.heightAnchor.constraint(equalToConstant: 50),
+            backButton.widthAnchor.constraint(equalToConstant: 150)
+        ])
+        
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func backButtonTapped() {
+        self.dismiss(animated: true)
+    }
+}
---
+++ WBChat/Дополнительно💻/StartPointViewController.swift
@@ -0,0 +1,91 @@
+//
+//  StartPointViewController.swift
+//  WBChat
+//
+//  Created by Александр on 12.06.2024.
+//
+
+import Foundation
+import UIKit
+
+final class StartPointViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+        setupViews()
+    }
+    
+    let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Перейти на VC2", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemBlue
+        button.layer.masksToBounds = true
+        return button
+    }()
+    
+    private func setupViews() {
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.heightAnchor.constraint(equalToConstant: 50),
+            button.widthAnchor.constraint(equalToConstant: 150)
+
+        ])
+        
+        button.layer.cornerRadius = button.frame.height / 2
+        
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func buttonTapped() {
+        let vc = DestinationVC()
+        vc.modalPresentationStyle = .custom
+        vc.transitioningDelegate = self
+        present(vc, animated: true)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        button.layer.cornerRadius = button.frame.size.height / 2
+    }
+}
+
+
+extension UIViewController: UIViewControllerTransitioningDelegate {
+    public func animationController(
+        forPresented presented: UIViewController,
+        presenting: UIViewController,
+        source: UIViewController
+    ) -> (
+        any UIViewControllerAnimatedTransitioning
+    )? {
+        RectangleAnimator(duration: 1.5, transitionMode: .present)
+    }
+    
+    public func animationController(forDismissed dismissed: UIViewController) -> (any UIViewControllerAnimatedTransitioning)? {
+        RectangleAnimator(duration: 1.5, transitionMode: .dismiss)
+    }
+}
+
+
+import SwiftUI
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+
+    func makeUIViewController(context: Context) -> some UIViewController {
+        StartPointViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+
+    }
+}
+
+struct ViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        ViewControllerRepresentable()
+            .ignoresSafeArea()
+    }
+}
