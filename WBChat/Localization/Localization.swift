2663ed45cc33871b8f5196e720249ad53b8b712c
QqewDev <kistalexander@yandex.ru>
2024-07-03 21:30:40 +0400

    Создан файл локализации
    Добавлены переводы текста на русский, английский, казахский и армянский языки

---
+++ WBChat/Localization/Localization.swift
@@ -0,0 +1,21 @@
+//
+//  Localization.swift
+//  WBChat
+//
+//  Created by Александр on 03.07.2024.
+//
+
+import SwiftUI
+
+enum Localization: LocalizedStringKey {
+    case closeText
+    case onboardingTitleText
+    case startChattingText
+    case userAgreementText
+    case inputPhoneText
+    case inputPhoneDescriptionText
+    case continueText
+    case searchPlaceholderText
+    
+    static let tableName = "AppLocalization"
+}
