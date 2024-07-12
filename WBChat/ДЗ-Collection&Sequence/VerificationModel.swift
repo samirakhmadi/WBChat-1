a3aadbf84da9093919351299ec459557fa8c6faa
QqewDev <kistalexander@yandex.ru>
2024-07-12 23:30:24 +0400

    Расширен функционал VerificationModel
    VerificationModel реализует протокол Sequence. В подсказке каждая вторая цифра пароля будет закрываться "*".

--- WBChat/ДЗ-Collection&Sequence/VerificationModel.swift
+++ WBChat/ДЗ-Collection&Sequence/VerificationModel.swift
@@ -7,16 +7,44 @@
 
 import Foundation
 
-struct VerificationModel: Hashable {
+struct VerificationModel {
     var phoneNumber: String
-    private var verificationCode: String
-
+    private(set) var verificationCode: [Character]
+    
     init(phoneNumber: String) {
         self.phoneNumber = phoneNumber
-        self.verificationCode = String(format: "%04d", Int.random(in: 0..<9999))
+        self.verificationCode = String(format: "%04d", Int.random(in: 0..<10000)).map { $0 }
     }
-
+    
     func validate(code: String) -> Bool {
-        return code == verificationCode
+        return String(verificationCode) == code
+    }
+    
+    mutating func regenerateVerificationCode() {
+        verificationCode = String(format: "%04d", Int.random(in: 0..<10000)).map { $0 }
+    }
+}
+
+extension VerificationModel: Sequence {
+    func makeIterator() -> some IteratorProtocol {
+        VerificationIterator(code: verificationCode)
+    }
+}
+
+extension VerificationModel: Hashable {}
+
+struct VerificationIterator: IteratorProtocol {
+    private var code: [Character]
+    private var index = 0
+    
+    init(code: [Character]) {
+        self.code = code
+    }
+    
+    mutating func next() -> Character? {
+        guard index < code.count else { return nil }
+        let char = index % 2 == 0 ? code[index] : "*"
+        index += 1
+        return char
     }
 }
